bdc169f256b5f2b305f7e4629ab65c203f3757c1
Krish Mittal <183262050+krish-mittal1@users.noreply.github.com>
2026-04-15 12:29:44 +0000

    feat: add docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # CORS â€” allow frontend dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173" , "http://127.0.0.1:5174"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
